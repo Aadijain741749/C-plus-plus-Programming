31e783b4ac42e2c7c53a7e0a7186d594f5de08af
Aadi Jain <258081268+Aadijain741749@users.noreply.github.com>
2026-07-06 14:54:07 +0530

    Add files via upload

---
+++ 01_First_C++_Program/code.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Aadi\nJain";
+  return 0;
+}
+
+// Boilerplate code
+// for compile the code we use
+//1. g++ code.cpp -o code
+//2. .\code.exe
