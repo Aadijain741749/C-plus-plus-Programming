66acc62d248c50b7fadefc9c2586dad0ba06ac18
Aadi Jain <258081268+Aadijain741749@users.noreply.github.com>
2026-07-07 22:23:17 +0530

    Add files via upload

---
+++ 05_Functions/01_Function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// Function definition
+void printHello(){
+  cout <<"Hello\n"; 
+}
+
+int main() {
+  //function call / invoke
+  printHello();
+  printHello();
+  printHello();
+  printHello();
+  printHello();
+  printHello();
+
+  return 0;
+  }
