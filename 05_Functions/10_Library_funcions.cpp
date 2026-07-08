0985c867d36661c9c928cc3a08bd166f448dc1a3
Aadi Jain <258081268+Aadijain741749@users.noreply.github.com>
2026-07-08 23:05:41 +0530

    Add files via upload

---
+++ 05_Functions/10_Library_funcions.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+  cout << sqrt(4) << endl; // library function
+  cout << cbrt(27) << endl; // library function
+  cout << min(4,8) << endl; // library function
+  cout << max(4,8) << endl; // library function
+  cout << pow(2,10) << endl; // library function
+}
